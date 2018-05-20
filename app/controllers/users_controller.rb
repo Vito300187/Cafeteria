5bb940ec95cbc3678fc4fa44073dcece83b35286
Vitalii Kuzminov <repz2010@gmail.com>
2018-05-20 23:14:36 +0300

    Fixed a bug in the controller user - params.require(:user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:users).permit( :name, :email, :password )
+    params.require(:user).permit( :name, :email, :password )
   end
 end
