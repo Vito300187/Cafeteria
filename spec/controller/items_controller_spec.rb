00b2f772cda82d30514456a2a063a9329dd24afc
Vitalii Kuzminov <repz2010@gmail.com>
2018-05-21 16:39:04 +0300

    change link in readme

--- spec/controller/items_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-describe ItemsController do
-  describe 'show' do
-    it 'renders show template if an item is found' do
-
-    end
-  end
-end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'anonymous' do
+    it 'kick from #show' do
+      get 'show', id:1
+      expect(response.status).not_to eq 200
+      expect(response).redirect_to(new_user_session_path)
+    end
+  end
 end
